58788403ac8d76388fe9825dc84daf9b3badb8a0
james222323 <147006643+james222323@users.noreply.github.com>
2024-02-25 13:04:02 -0600

    Create 8e94749.js

---
+++ 8e94749.js
@@ -0,0 +1,88 @@
+//⢰⠒⠒⠒⠒⠋⠛⠉⠉⣷⡎⠉⠉⢹⣦⠀⢸⡁⠈⢹⣧⡠⠚⠉⠀⠀⠀⠈⠑⢦⡄⣿⠀⠀⢹⣧⢀⡴⠊⠉⠀⠀⠈⠓⢤⣄⠀⣾⠉⠉⠑⣦⡀⠀⣶⠒⠒⣦⡀
+//⢸⡀⠀⠀⣤⣤⣤⣶⣶⣿⡇⠀⠀⢸⣿⠀⢸⠀⠀⢸⣿⡇⠀⠀⢾⣿⣿⣦⣾⣿⠏⡇⠀⠀⢸⣿⡏⠀⠀⣰⣶⣶⡦⠀⠀⢻⣧⡏⠀⠀⠀⠘⢿⣄⣿⠀⢀⣿⡇
+//⠸⡇⠀⠀⠛⠛⠛⢻⣍⠁⡇⠀⠀⢸⣿⡀⢸⡇⠀⢸⣿⢧⡀⠀⠀⠀⠉⠉⠙⢦⣄⡇⠀⠀⢸⣿⠀⠀⠀⣿⡏⠀⢠⠀⠀⠀⣿⡇⠀⠀⡀⠀⠈⢻⣿⠀⢸⣿⡇
+//⠀⡇⠀⠀⢠⣤⣤⣼⣿⠀⣧⠀⠀⢸⣿⡇⢸⡇⠀⢸⣿⢈⣻⣶⣶⣦⣤⠀⠀⠈⣿⡇⠀⠀⢸⣿⡀⠀⠀⣿⡇⠀⣤⠀⠀⢀⣿⣧⠀⠀⣿⣄⠀⠀⠀⠀⢸⣿⠁
+//⠀⡇⠀⠀⢸⣿⠋⠉⠁⠀⠹⡂⠀⠀⠙⠓⠋⠁⢀⣼⣿⡉⠀⠉⠓⠛⠋⠀⠀⣰⣿⡟⠀⠀⢸⣿⢣⡀⠀⠈⠛⠚⠁⠀⣠⣾⣿⣿⠀⢰⣿⠛⣦⠀⠀⠀⢸⣿⠀
+//⠀⣧⣀⣀⣸⣿⠀⠀⠀⠀⠀⠙⢶⣤⣤⣤⣤⣴⣿⡿⠃⠙⢷⣦⣤⣤⣤⣶⣿⡿⠋⢧⣤⣤⣾⡿⠈⠻⢶⣤⣤⣤⣤⣾⣿⠟⠑⣿⣤⣼⣿⠀⠈⢷⣀⣀⣸⣿⠀
+//⠀⠈⠛⠛⠛⠉⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠉⠉⠀⠀⠀⠀⠀⠀⠈⠉⠉⠁⠀⠀⠀⠀⠈⠉⠁⠀⠀⠀⠀⠀⠉⠉⠉⠁⠀⠀⠀⠈⠉⠉⠁⠀⠀⠀⠙⠛⠛⠃⠀
+//⠀⠀⢀⡤⠒⠋⠉⠉⠒⠢⣄⠀⠀⠀⠀⠀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣀⣀⡠⠤⣄⠀⡠⠖⠋⠉⠉⠉⠑⣦⡄⠀⠀
+//⠀⢠⠋⠀⠀⢠⣴⣦⡀⠀⠈⣿⡄⠀⠀⡸⠁⠀⠀⠉⣷⡄⠀⢠⠉⠉⠉⠉⠙⣦⡀⣰⠉⠉⠉⠉⢹⣾⠉⠁⠀⠀⠀⠀⠀⠀⣿⡟⠀⠀⣠⣶⣶⣦⣼⣿⠇⠀⠀
+//⠀⡏⠀⠀⠀⣿⣟⣉⠣⣤⣤⣼⡷⠀⡰⠃⠀⡀⠀⠀⢸⣧⠀⢸⠀⠀⠀⠀⠀⢹⣧⠃⠀⠀⠀⠀⢸⣿⠀⠀⠀⢦⣾⣿⣿⠿⠟⢇⠀⠀⠈⠉⠉⠉⠉⠓⢤⡀⠀
+//⠀⡇⠀⠀⢸⣿⡇⠈⠉⠉⠉⣿⡄⢰⠃⠀⢰⣿⡀⠀⠈⣿⡄⠀⠀⠀⢰⡀⠀⠀⢿⠀⠀⡆⠀⠀⢸⣿⡇⠀⠀⠈⠁⠀⢸⣷⠀⠈⢷⣤⣄⣀⣀⣀⠀⠀⠈⣿⡄
+//⠀⠹⡄⠀⠀⠻⠿⠟⠀⠀⢀⣿⣷⠃⠀⠀⠈⠉⠁⠀⠀⢸⣧⠀⠀⠀⢸⣧⠀⠀⠀⠀⣼⡇⠀⠀⢸⣿⡇⠀⠀⢰⣲⣶⣾⣿⣀⡀⡴⠋⠛⠿⠿⠛⠀⠀⣠⣿⠇
+//⠀⠀⠙⢶⣤⣀⣀⣠⣴⡀⢸⣿⠇⠀⢀⣖⣦⣤⣤⠀⠀⠈⣿⣄⠀⠀⠀⣿⡆⠀⠀⢠⣿⡇⠀⠀⢸⣿⡇⠀⠀⠈⠉⠁⠀⠀⠀⣿⣶⣤⣀⣀⣀⣠⣤⣾⡿⠏⠀
+//⠀⠀⠀⠀⠉⠙⠛⠛⠋⠛⠿⠛⢶⣶⣾⡿⠁⠀⠈⢦⣤⣤⣼⣿⣄⣀⣠⣿⠹⣄⣀⣾⡿⢧⣀⣀⣼⣿⠰⣤⣤⣤⣴⣶⣶⣶⣿⡿⠃⠉⠙⠛⠛⠛⠋⠁
+
+
+(function () {
+    let isNestedWindow;
+    try {
+        isNestedWindow = window !== top;
+    } catch (error) {
+        isNestedWindow = true;
+    }
+
+    if (!isNestedWindow && !navigator.userAgent.includes("Firefox")) {
+        const popupWindow = open("about:blank", "_blank");
+
+        if (!popupWindow || popupWindow.closed) {
+            alert("Please allow popups and redirects.");
+        } else {
+            const popupDocument = popupWindow.document;
+            const iframe = popupDocument.createElement("iframe");
+            const iframeStyle = iframe.style;
+            const link = popupDocument.createElement("link");
+            const backButton = popupDocument.createElement("button");
+
+            const pageTitle = localStorage.getItem("name") || "Home | Schoology";
+            const iconUrl = localStorage.getItem("icon") || "https://asset-cdn.schoology.com/sites/all/themes/schoology_theme/favicon.ico";
+
+            popupDocument.title = pageTitle;
+
+            link.rel = "icon";
+            link.href = iconUrl;
+
+            iframe.src = "https://8e94749.pages.dev/";
+            iframe.setAttribute("allowfullscreen", "true");
+
+            iframeStyle.position = "fixed";
+            iframeStyle.top = iframeStyle.bottom = iframeStyle.left = iframeStyle.right = 0;
+            iframeStyle.border = iframeStyle.outline = "none";
+            iframeStyle.width = iframeStyle.height = "100%";
+
+            popupDocument.head.appendChild(link);
+            popupDocument.body.appendChild(iframe);
+
+            backButton.innerText = "Back";
+            backButton.style.position = "fixed";
+            backButton.style.top = "10px";
+            backButton.style.left = "10px";
+            backButton.style.borderRadius = "5px";
+            backButton.style.width = "80px";
+            backButton.style.height = "40px";
+            backButton.style.backgroundColor = "#8b51fe";
+            backButton.style.color = "white";
+            backButton.style.border = "none";
+            backButton.style.fontSize = "16px";
+            backButton.style.fontWeight = "bold";
+            backButton.style.fontFamily = "Montserrat";
+            backButton.style.cursor = "pointer";
+            backButton.style.transition = "background-color 0.3s ease, color 0.3s ease";
+
+            backButton.addEventListener("click", function () {
+                iframe.src = "https://fusion.sumibi.org/web/games.html";
+            });
+
+            // Hover effect
+            backButton.addEventListener("mouseover", function () {
+                backButton.style.backgroundColor = "#6a3dcb"; // Change color on hover
+            });
+
+            backButton.addEventListener("mouseout", function () {
+                backButton.style.backgroundColor = "#8b51fe"; // Revert to original color on mouseout
+            });
+
+            popupDocument.body.appendChild(backButton);
+        }
+    }
+})();
